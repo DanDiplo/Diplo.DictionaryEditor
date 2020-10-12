bd1ac608b73f6d36ed24fc9baa8297d468210dbf
Tobias Lopez <tobias.lopez@optenag.onmicrosoft.com>
2020-10-12 16:08:09 +0200

    fix: allow multiline cells with new package CsvHelper

---
+++ Diplo.Dictionary.Tests/CsvServiceTests.cs
@@ -0,0 +1,84 @@
+﻿using Diplo.Dictionary.Models;
+using Diplo.Dictionary.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Diplo.Dictionary.Tests
+{
+	[TestClass]
+	public class CsvServiceTests
+	{
+		private readonly CsvService _service;
+
+		public CsvServiceTests()
+		{
+			_service = new CsvService();
+		}
+
+		[TestMethod]
+		public void Should_Parse_With_Multiline_Cells()
+		{
+
+			string source = $"Id,Key,LangId,Language,Name,Translation{Environment.NewLine}" +
+				$"1991,f1900d0e-ba19-45c7-87ca-bec18c03dc80,2,French (Switzerland),Dictionary.Key,\"Lorem ipsum dolor sit amet. {Environment.NewLine}" +
+				"consectetur adipiscing elit\"";
+
+			ValidationResponse validationResponse = _service.ValidateCsv(source);
+
+			Assert.IsTrue(validationResponse.IsValid);
+			Assert.AreEqual(1, validationResponse.RowsFound);
+		}
+
+		[TestMethod]
+		public void Fails_Cause_Id_Invalid()
+		{
+			string source = $"Id,Key,LangId,Language,Name,Translation{Environment.NewLine}" +
+				$"asdf,f1900d0e-ba19-45c7-87ca-bec18c03dc80,2,French (Switzerland),Dictionary.Key,\"Lorem ipsum dolor sit amet. {Environment.NewLine}" +
+				"consectetur adipiscing elit\"";
+
+			ValidationResponse validationResponse = _service.ValidateCsv(source);
+
+			Assert.IsFalse(validationResponse.IsValid);
+		}
+
+		[TestMethod]
+		public void Fails_Cause_Guid_Invalid()
+		{
+			string source = $"Id,Key,LangId,Language,Name,Translation{Environment.NewLine}" +
+				$"1991,123,2,French (Switzerland),Dictionary.Key,\"Lorem ipsum dolor sit amet. {Environment.NewLine}" +
+				"consectetur adipiscing elit\"";
+
+			ValidationResponse validationResponse = _service.ValidateCsv(source);
+
+			Assert.IsFalse(validationResponse.IsValid);
+		}
+
+		[TestMethod]
+		public void Fails_Cause_LangId_Invalid()
+		{
+			string source = $"Id,Key,LangId,Language,Name,Translation{Environment.NewLine}" +
+				$"1991,f1900d0e-ba19-45c7-87ca-bec18c03dc80,asdf,French (Switzerland),Dictionary.Key,\"Lorem ipsum dolor sit amet. {Environment.NewLine}" +
+				"consectetur adipiscing elit\"";
+
+			ValidationResponse validationResponse = _service.ValidateCsv(source);
+
+			Assert.IsFalse(validationResponse.IsValid);
+		}
+
+		[TestMethod]
+		public void Simple_Parsing_Test()
+		{
+			string source = $"Id,Key,LangId,Language,Name,Translation{Environment.NewLine}" +
+				$"1991,f1900d0e-ba19-45c7-87ca-bec18c03dc80,2,French (Switzerland),Dictionary.Key,\"Lorem ipsum dolor sit amet.\"{Environment.NewLine}" +
+				$"1991,f1900d0e-ba19-45c7-87ca-bec18c03dc80,2,French (Switzerland),Dictionary.Key,\"Lorem ipsum dolor sit amet.\"{Environment.NewLine}" +
+				$"1991,f1900d0e-ba19-45c7-87ca-bec18c03dc80,2,French (Switzerland),Dictionary.Key,\"Lorem ipsum dolor sit amet.\"{Environment.NewLine}" +
+				$"1991,f1900d0e-ba19-45c7-87ca-bec18c03dc80,2,French (Switzerland),Dictionary.Key,\"Lorem ipsum dolor sit amet.\"{Environment.NewLine}" +
+				$"1991,f1900d0e-ba19-45c7-87ca-bec18c03dc80,2,French (Switzerland),Dictionary.Key,\"Lorem ipsum dolor sit amet.\"";
+
+			ValidationResponse validationResponse = _service.ValidateCsv(source);
+
+			Assert.IsTrue(validationResponse.IsValid);
+			Assert.AreEqual(5, validationResponse.RowsFound);
+		}
+	}
+}
---
+++ Diplo.Dictionary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Diplo.Dictionary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Diplo.Dictionary.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2841b225-39de-4d88-b1f9-663055fed616")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
